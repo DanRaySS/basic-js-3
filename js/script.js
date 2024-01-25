f63a99058db9bcabe8887501b18613c9b98b3a71
Daniil <example@gmail.com>
2024-01-25 20:13:27 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = `5`;
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.age);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+
+console.log(arr[0]);
