2dc0a8bdab1b1a5d9af5fa4ad229df2f5496aff2
Daniil <danilmux@gmail.com>
2024-02-17 21:36:16 +0500

    async, defer, dynamic scripts

--- js/script.js
+++ js/script.js
@@ -838,3 +838,18 @@ P.S. Здесь есть несколько вариантов решения з
 
 
 //
+
+const p = document.querySelectorAll('p');
+console.log(p);
+
+
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+
+loadScript("js/test.js");
+loadScript("js/some.js");
---
+++ js/test.js
@@ -0,0 +1,16 @@
+const obj = {
+    library: 'closed',
+    cafe: 'open',
+    boolean: true
+};
+
+const arr = [1, 2, 3, '', '45', true];
+
+const { library } = obj;
+
+const [first, second] = arr;
+
+console.log('Test');
+console.log(library);
+console.log(first);
+console.log(second);
